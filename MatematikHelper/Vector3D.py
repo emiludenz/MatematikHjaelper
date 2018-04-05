bd0b1cd405184ae9cbde3b4e5ef4f42a67b946d6
emiludenz <emilbh@gmail.com>
2018-04-05 17:38:26 +0200

    Still trying

--- MatematikHelper/Vector3D.py
+++ MatematikHelper/Vector3D.py
@@ -2,7 +2,7 @@ from math import sqrt as sq
 from math import acos, degrees
 
 class Vector3D:
-    def __init__(self, x=0, y=0, z=0):
+    def __init__(self, x=0.0, y=0.0, z=0.0):
         self.x = x
         self.y = y
         self.z = z
@@ -41,17 +41,33 @@ class Vector3D:
         z = self.z * other_vec.z
         return x+y+z
 
-    def lengde(self):
-        lengde = (self.x**2)+(self.y**2)+(self.z**2)
-        return sq(lengde)
+    def lengde(self,vec=0):
+        if vec == 0:
+            lengde = (self.x ** 2) + (self.y ** 2) + (self.z ** 2)
+            return sq(lengde)
+        else:
+            lengde = (vec.x-self.x ) ** 2 + ( vec.y-self.y) ** 2 + ( vec.z-self.z) ** 2
+            return sq(lengde)
 
     def areal_parallellogram(self, vec=0):
+        if vec == 0:
+            v3 = self.krydsprodukt()
+            return v3.lengde()
+        else:
+            other_vec = vec
+            v3 = self.krydsprodukt(other_vec)
+            return v3.lengde(other_vec)
+
+    def projektering(self,vec=0):
         if vec == 0:
             other_vec = Vector3D()
         else:
             other_vec = vec
-        v3 = self.krydsprodukt(other_vec)
-        return v3.lengde()
+        skalar = self.skaler_produkt(other_vec)
+        legnde = other_vec.lengde()
+        t = skalar/legnde
+        newVec = Vector3D(t * other_vec.x, t * other_vec.y, t * other_vec.z)
+        return newVec.lengde()
 
     def vinkel(self,vec=0):
         if vec == 0:
@@ -66,11 +82,10 @@ class Vector3D:
 
     def krydsprodukt(self,vec=0):
         if vec == 0:
-            other_vec = Vector3D()
+            other_vec = Vector3D(0,0,0)
         else:
             other_vec = vec
-
         x = self.y*other_vec.z - self.z*other_vec.y
-        y = (self.x*other_vec.z - self.z*other_vec.x)*-1
-        z = self.z*other_vec.x - self.y*other_vec.x
+        y = (self.z*other_vec.x - self.x*other_vec.z)
+        z = self.x*other_vec.y - self.y*other_vec.x
         return Vector3D(x,y,z)
--- MatematikHelper/main.py
+++ MatematikHelper/main.py
@@ -1,4 +1,5 @@
 import Vector3D as V3D
+from math import sqrt as sq
 #import Vectors as V
 #import opgaver
 #import GraphFun as GF
@@ -7,56 +8,6 @@ import Vector3D as V3D
 
 
 def main():
-    # a(3t-1,12)
-    # b(6,t+8)
-    #opgaver.opgave1()
-    """
-    print("__________Example______________")
-    a = V.Vector2D(1,6)
-    b = V.Vector2D(5,2)
-    c = a.projektering(b)
-    print("X:{}\nY:{}".format(c.x,c.y))
-
-    print("____________Opgave 1.26____________")
-    a = V.Vector2D(1,2)
-    b = V.Vector2D(4,1)
-    c = a.projektering(b)
-    print("a)\nX:{}\nY:{}\n".format(c.x,c.y))
-
-    a = V.Vector2D(-3,-1)
-    b = V.Vector2D(-5,4)
-    c = a.projektering(b)
-    print("b)\nX:{}\nY:{}\n".format(c.x,c.y))
-    """
-    """
-    a = V.Vector2D(-6,3)
-    b = V.Vector2D(10,4)
-    c = V.Vector2D(1,10)
-    d = V.Vector2D(20,5)
-    L = [a,b,c,d]
-
-    V.show_vectors(L)
-    """
-
-    """
-    l = GF.AndenGrads("-3*x**2+6*x+2")
-    l.differentabel()
-    """
-
-    """
-    b = a.tver_vektor()
-    print("Vx:{}  Vy:{}".format(b.x,b.y))
-    """
-
-    #opgaver.opgave2()
-    #x = Symbol("x")
-
-    """
-    f = Fun.Function("20+150*log(8*x+1)")
-    f.show_function()
-
-    #f.show_function(range(-100,100))
-    
     """
     v1 = V3D.Vector3D(1,2,-3)
     v2 = V3D.Vector3D(-3,12,-8)
@@ -69,12 +20,12 @@ def main():
     v1 = V3D.Vector3D(4,2,-3)
     v2 = V3D.Vector3D(0,-9,6)
     print(v1.vinkel(v2))
- 
+
     v1 = V3D.Vector3D(4, 2, -3)
     v2 = V3D.Vector3D(0, -9, 6)
     v3=v1.krydsprodukt(v2)
     print(v3.out())
-
+    """
 
     v1 = V3D.Vector3D(-1,-4,-6)
     v2 = V3D.Vector3D(5,-1,8)
@@ -85,9 +36,13 @@ def main():
     v2 = V3D.Vector3D(-3,0,8)
     print(v1.areal_parallellogram(v2))
 
+
     v1 = V3D.Vector3D(1,7,-5)
     v2 = V3D.Vector3D(3,10,-7)
     print(v1.areal_parallellogram(v2))
+
+
+
 
     return 0
 
