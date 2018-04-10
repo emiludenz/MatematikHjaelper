84f1908323b04b270be09fde59d5c6e79b5c2bde
emiludenz <emilbh@gmail.com>
2018-04-10 20:38:56 +0200

    Added plan calculations for 3D space

--- trekanter.py
+++ trekanter.py
@@ -12,26 +12,32 @@ class Trekant:
         self.A = A
         self.rect = self.checkIsRet()
 
-
     def checkIsRet(self):
         if self.a ** +self.b ** 2 == self.c ** 2:
             return True
         else:
             return False
 
-    def getAngels(self):
+    def getAngles(self):
         if self.rect:
-            pass
+            if self.b > 0 and self.c > 0:
+                cosA = self.b/self.c
+                self.A = degrees(acos(cosA))
+
         else:
             # getting angle C
             cosC = (self.a**2+self.b**2-self.c**2)/(2*self.a*self.b)
             self.C = degrees(acos(cosC))
-            self.Crad = acos(cosC)
+            self.C_rad = acos(cosC)
             # getting angel B
-            sinB = (self.b*sin(self.Crad))/self.c
+            sinB = (self.b*sin(self.C_rad))/self.c
             self.B = degrees(asin(sinB))
+            self.B_rad = asin(sinB)
             # getting angel A
-            sinA = (self.a * sin(self.Crad)) / self.c
+            sinA = (self.a * sin(self.C_rad)) / self.c
             self.A = degrees(asin(sinA))
+            self.A_rad = asin(sinA)
+
+
 
 
